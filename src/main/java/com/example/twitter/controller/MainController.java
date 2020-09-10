ce248735e026724700100e33fe7c6a44bc7496ce
Vitalii Rysich <vitalii.rysich@efl.com.pl>
2020-09-10 23:50:36 +0200

    Authorization, registration

--- src/main/java/com/example/twitter/controller/MainController.java
+++ src/main/java/com/example/twitter/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.example.twitter;
+package com.example.twitter.controller;
 
 import com.example.twitter.domain.Message;
 import com.example.twitter.repos.MessageRepo;
@@ -13,19 +13,17 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-public class GreetingController {
+public class MainController {
 
     @Autowired
     private MessageRepo messageRepo;
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name,
-                           Map<String, Object> model) {
-        model.put("name", name);
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
         return "greeting";
     }
 
-    @GetMapping
+    @GetMapping("/main")
     public String main(Map<String, Object> model){
         Iterable<Message> messages = messageRepo.findAll();
 
@@ -33,7 +31,7 @@ public class GreetingController {
         return "main";
     }
 
-    @PostMapping("add")
+    @PostMapping("/main")
     public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model){
         Message message = new Message(text,tag);
         messageRepo.save(message);
